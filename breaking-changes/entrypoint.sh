67edd0a8993092a6215e05896f6eff6e2d5c0fee
effoeffi <effoeffi@gmail.com>
2023-05-13 13:13:39 +0300

    init

---
+++ breaking-changes/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+readonly base="$1"
+readonly revision="$2"
+readonly fail_on_diff="$6"
+
+echo "running oasdiff check for breaking-changes... base: $base, revision: $revision, fail_on_diff: $fail_on_diff"
+
+if [ "$fail_on_diff" = "true"  ]
+then
+  oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
+else
+then
+  oasdiff -check-breaking -base "$base" -revision "$revision"
+fi
