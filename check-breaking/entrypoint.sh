c4c8d1613986aee5a28a65ccf26407ac51decee5
Effi <effoeffi@users.noreply.github.com>
2023-06-08 18:32:27 +0300

    Fix fail on diff; default fail on diff for breaking changes - changed to true (#6)

--- check-breaking/entrypoint.sh
+++ check-breaking/entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-set -e
 
 readonly base="$1"
 readonly revision="$2"
 readonly fail_on_diff="$3"
 
 echo "running oasdiff check for breaking-changes... base: $base, revision: $revision, fail_on_diff: $fail_on_diff"
-
-if [ "$fail_on_diff" = "true"  ]
-then
-  oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
-else
-  oasdiff -check-breaking -base "$base" -revision "$revision"
+oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
+if [ $? != 0 ] && [ "$fail_on_diff" = "true"  ]; then
+  exit 1
 fi
