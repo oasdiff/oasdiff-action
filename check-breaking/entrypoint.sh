259938997150d05ba9c828bcf9a73f2b8f27afe3
Nicolás San Martín <57573579+nicolassanmar@users.noreply.github.com>
2023-06-08 15:23:54 -0300

    Feat: add -fail-on-warns support (#7)

--- check-breaking/entrypoint.sh
+++ check-breaking/entrypoint.sh
@@ -3,9 +3,13 @@
 readonly base="$1"
 readonly revision="$2"
 readonly fail_on_diff="$3"
+readonly fail_on_warns="$4"
 
-echo "running oasdiff check for breaking-changes... base: $base, revision: $revision, fail_on_diff: $fail_on_diff"
-oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
+echo "running oasdiff check for breaking-changes... base: $base, revision: $revision, fail_on_diff: $fail_on_diff, fail_on_warns: $fail_on_warns"
+
+readonly fail_on_warns_argument=$(if [ "$fail_on_warns" = "true" ]; then echo "-fail-on-warns"; fi)
+
+oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision" "$fail_on_warns_argument"
 if [ $? != 0 ] && [ "$fail_on_diff" = "true"  ]; then
   exit 1
 fi
