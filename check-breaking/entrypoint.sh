f874e300e18a690dd6d5d13178f81b1287a53008
effoeffi <effoeffi@gmail.com>
2023-06-08 18:18:31 +0300

    fix fail on breaking

--- check-breaking/entrypoint.sh
+++ check-breaking/entrypoint.sh
@@ -6,10 +6,7 @@ readonly revision="$2"
 readonly fail_on_diff="$3"
 
 echo "running oasdiff check for breaking-changes... base: $base, revision: $revision, fail_on_diff: $fail_on_diff"
-
-if [ "$fail_on_diff" = "true"  ]
-then
-  oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
-else
-  oasdiff -check-breaking -base "$base" -revision "$revision"
+oasdiff -check-breaking -fail-on-diff -base "$base" -revision "$revision"
+if [ "$fail_on_diff" = "true"  ] && [ $? != 0 ]; then
+  exit 1
 fi
