e31a4fa4631e73de3b535fa080c3d70fb656394e
effoeffi <effoeffi@gmail.com>
2023-08-12 01:14:59 +0300

    use -z instead of -n

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -12,6 +12,9 @@ flags=""
 if [[ $fail_on_diff = "true" ]]; then
     flags+="--fail-on WARN "
 fi
+if [[ ! -z $include_checks ]]; then
+    flags+="--include-checks $include_checks "
+fi
 echo "flags: $flags"
 
 if [[ -n $flags ]]; then
