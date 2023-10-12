90fe3f65d94a3701cab2af8d3685a2cb5f1ebe93
Ivan Larin <pentusha@gmail.com>
2023-10-12 19:07:38 +0300

    Add --include-path-params flag for #16

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -4,13 +4,18 @@ set -e
 readonly base="$1" 
 readonly revision="$2" 
 readonly fail_on_diff="$3" 
-readonly include_checks="$4" 
-echo "running oasdiff breaking base: $base, revision: $revision, fail_on_diff: $fail_on_diff, include_checks: $include_checks" 
+readonly include_checks="$4"
+readonly include_path_params="$5"
+
+echo "running oasdiff breaking base: $base, revision: $revision, fail_on_diff: $fail_on_diff, include_checks: $include_checks, include_path_params: $include_path_params"
 
 # Build flags to pass in command
 flags=""
 if [ "$fail_on_diff" = "true" ]; then
     flags="${flags} --fail-on WARN"
+fi
+if [ "$include_path_params" = "true" ]; then
+    flags="${flags} --include-path-params"
 fi
 if [ -n "$include_checks" ]; then
     flags="${flags} --include-checks $include_checks"
