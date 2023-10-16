74fbf198e382fa4fd63f9dd47b166f007a432fca
Ivan Larin <Pentusha@users.noreply.github.com>
2023-10-16 20:58:40 +0300

    Add include-path-params flag (#17)
    * Add --include-path-params flag for #16
    
    * refer to original documentation in oasdiff
    
    * Add mapping for CLI argumetns to action's inputs #17
    
    Move format to flags for diff action
    
    ---------
    
    Co-authored-by: Reuven <rh@tufin.com>

--- changelog/entrypoint.sh
+++ changelog/entrypoint.sh
@@ -3,9 +3,21 @@ set -e
 
 readonly base="$1"
 readonly revision="$2"
+readonly include_path_params="$3"
 
-echo "running oasdiff changelog base: $base, revision: $revision"
+echo "running oasdiff changelog base: $base, revision: $revision, include_path_params: $include_path_params"
+
+# Build flags to pass in command
+flags=""
+if [ "$include_path_params" = "true" ]; then
+    flags="${flags} --include-path-params"
+fi
+echo "flags: $flags"
 
 set -o pipefail
 
-oasdiff changelog "$base" "$revision"
+if [ -n "$flags" ]; then
+    oasdiff changelog "$base" "$revision" $flags
+else
+    oasdiff changelog "$base" "$revision"
+fi
--- diff/entrypoint.sh
+++ diff/entrypoint.sh
@@ -5,13 +5,27 @@ readonly base="$1"
 readonly revision="$2"
 readonly format="$3"
 readonly fail_on_diff="$4"
+readonly include_path_params="$5"
 
-echo "running oasdiff diff base: $base, revision: $revision, format: $format, fail_on_diff: $fail_on_diff"
+echo "running oasdiff diff base: $base, revision: $revision, format: $format, fail_on_diff: $fail_on_diff, include_path_params: $include_path_params"
+
+# Build flags to pass in command
+flags=""
+if [ "$format" != "yaml" ]; then
+    flags="${flags} --format ${format}"
+fi
+if [ "$fail_on_diff" = "true" ]; then
+    flags="${flags} --fail-on-diff"
+fi
+if [ "$include_path_params" = "true" ]; then
+    flags="${flags} --include-path-params"
+fi
+echo "flags: $flags"
 
 set -o pipefail
 
-if [[ $fail_on_diff == "true" ]]; then
-  oasdiff diff "$base" "$revision" --fail-on-diff --format "$format" 
+if [ -n "$flags" ]; then
+    oasdiff diff "$base" "$revision" $flags
 else
-  oasdiff diff "$base" "$revision" --format "$format"
+    oasdiff diff "$base" "$revision"
 fi
