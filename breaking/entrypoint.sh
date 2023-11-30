e00d6545446e783e728cb3a97035a350cc4fd9f6
effoeffi <effoeffi@gmail.com>
2023-11-30 18:55:01 +0200

    init

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-readonly base="$1" 
-readonly revision="$2" 
-readonly fail_on_diff="$3" 
+readonly base="$1"
+readonly revision="$2"
+readonly fail_on_diff="$3"
 readonly include_checks="$4"
 readonly include_path_params="$5"
 readonly deprecation_days_beta="$6"
@@ -28,10 +28,23 @@ fi
 if [ -n "$deprecation_days_stable" ]; then
     flags="${flags} --deprecation-days-stable $deprecation_days_stable"
 fi
+flags="${flags} --format githubactions"
 echo "flags: $flags"
 
-if [ -n "$flags" ]; then
-    oasdiff breaking "$base" "$revision" $flags
-else
-    oasdiff breaking "$base" "$revision"
+### GITHUB_OUTPUT ###
+
+output=$(oasdiff breaking "$base" "$revision" $flags)
+# GitHub Actions limits output to 1MB, see: https://docs.github.com/en/actions/using-jobs/defining-outputs-for-jobs
+# We count bytes because unicode has multibyte characters
+size=$(echo "$output" | wc -c)
+if [ "$size" -ge "1000000" ]; then
+    echo "WARN: Diff exceeds the 1MB limit, truncating output..." >&2
+    output=$(echo "$output" | head -c $1000000)
 fi
+if [ -n "$output" ]; then
+    echo "$output" >>$GITHUB_OUTPUT
+else
+    echo "No API breaking changes" >>$GITHUB_OUTPUT
+fi
+
+### END GITHUB_OUTPUT ###
