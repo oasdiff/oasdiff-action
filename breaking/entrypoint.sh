3c17fd20facaadd25a4d3b1fb6b0e299e2a736cd
effoeffi <effoeffi@gmail.com>
2023-12-03 14:36:14 +0200

    test 4

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -38,7 +38,7 @@ echo $output_github_action_summary
 # *** github action step output ***
 
 # output name
-# echo "breaking" >>$GITHUB_OUTPUT
+echo "breaking" >>$GITHUB_OUTPUT
 # output=$(oasdiff breaking "$base" "$revision" $flags | head -n 1)
 # if [ -n "$output" ]; then
 #     echo "$output" >>$GITHUB_OUTPUT
