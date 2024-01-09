95a2ffb75e9a9237ea699eb0b908075fcdbca34a
effoeffi <effoeffi@gmail.com>
2024-01-09 17:56:28 +0200

    breaking: remove check for github-action limits output to 1MB

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -47,13 +47,6 @@ else
 fi
 
 if [ -n "$output" ]; then
-    # github-action limits output to 1MB
-    # we count bytes because unicode has multibyte characters
-    size=$(echo "$output" | wc -c)
-    if [ "$size" -ge "1000000" ]; then
-        echo "WARN: breaking exceeds the 1MB limit, truncating output..." >&2
-        output=$(echo "$output" | head -c $1000000)
-    fi
     echo "$output" >>$GITHUB_OUTPUT
 else
     echo "No breaking changes" >>$GITHUB_OUTPUT
