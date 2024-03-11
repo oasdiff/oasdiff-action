e32a01a4076a41a766ac335fafbf5fcf87fe6e5a
effoeffi <effoeffi@gmail.com>
2024-03-11 13:01:30 +0200

    t4

--- breaking/entrypoint.sh
+++ breaking/entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 set -e
 
-write_output () {
-    local output="$1"
-    if [ -n "$output_to_file" ]; then
-        local file_output="$2"
-        if [ -z "$file_output" ]; then
-            file_output=$output
-        fi
-        echo "$file_output" >> "$output_to_file"
-    fi
-    # github-action limits output to 1MB
-    # we count bytes because unicode has multibyte characters
-    size=$(echo "$output" | wc -c)
-    if [ "$size" -ge "1000000" ]; then
-        echo "WARN: diff exceeds the 1MB limit, truncating output..." >&2
-        output=$(echo "$output" | head -c 1000000)
-    fi
-    echo "$output" >>"$GITHUB_OUTPUT"
-}
+# write_output () {
+#     local output="$1"
+#     if [ -n "$output_to_file" ]; then
+#         local file_output="$2"
+#         if [ -z "$file_output" ]; then
+#             file_output=$output
+#         fi
+#         echo "$file_output" >> "$output_to_file"
+#     fi
+#     # github-action limits output to 1MB
+#     # we count bytes because unicode has multibyte characters
+#     size=$(echo "$output" | wc -c)
+#     if [ "$size" -ge "1000000" ]; then
+#         echo "WARN: diff exceeds the 1MB limit, truncating output..." >&2
+#         output=$(echo "$output" | head -c 1000000)
+#     fi
+#     echo "$output" >>"$GITHUB_OUTPUT"
+# }
 
 readonly base="$1"
 readonly revision="$2"
